d8bb9950fa92e28dea81ab40726d8910b12637c3
UCHIDA Yusuke <>
2021-11-18 14:33:56 +0900

    割合を変更できるようにするためにテストを追加

--- src/test/java/warikan/domain/model2/WaikanTest.java
+++ src/test/java/warikan/domain/model2/WaikanTest.java
@@ -15,4 +15,15 @@ public class WaikanTest {
     assertEquals(warikan.medium, 10000);
     assertEquals(warikan.small, 5000);
   }
+
+  @Test
+  public void 傾斜1_2対1対0_8で合計30000円() {
+    Nomikai nomikai = new Nomikai();
+
+    Warikan warikan = nomikai.calculate(30000);
+
+    assertEquals(warikan.large, 12000);
+    assertEquals(warikan.medium, 10000);
+    assertEquals(warikan.small, 8000);
+  }
 }
