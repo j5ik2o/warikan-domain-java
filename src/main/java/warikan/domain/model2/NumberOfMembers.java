e6223ab188cb43f10068e58fb7d6f4ca95cb94cb
UCHIDA Yusuke <>
2021-11-18 15:50:54 +0900

    人数のバリデーションを追加

--- src/main/java/warikan/domain/model2/NumberOfMembers.java
+++ src/main/java/warikan/domain/model2/NumberOfMembers.java
@@ -6,6 +6,9 @@ public class NumberOfMembers {
   int small;
 
   public NumberOfMembers(int large, int medium, int small) {
+    if (large < 0 || medium < 0 || small < 0) {
+      throw new IllegalArgumentException("パラメータが不正です");
+    }
     this.large = large;
     this.medium = medium;
     this.small = small;
